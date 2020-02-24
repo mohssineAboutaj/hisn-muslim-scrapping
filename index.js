726194e7716ffcbbd93c480a8f5c2ae2b1493ade
mohssineAboutaj <mohssine@mohssine-HP-Compaq-6200.(none)>
2020-02-24 15:10:51 +0100

    add message to tell the user wait the proccessing

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const main = require('./main.json').MAIN,
 			dest = "./data/",
 			{ pathExist } = require('./global');
 
+console.log("The proccessing will take some time over than 5 minute, please wait until everything done");
+
 pathExist(dest)
 
 main.forEach(async function(el) {
